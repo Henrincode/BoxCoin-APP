b84d415bf7f30533bf1961b6b9c954969df1ca4d
Henrique <spellctro@gmail.com>
2026-05-27 20:19:58 -0300

    style: creating colors theme

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,22 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
     return (
-        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-            <Text style={{ fontSize: 60 }}>Olá mundo!</Text>
+        <View style={s.container}>
+            <Text style={s.Text}>Olá mundo!</Text>
         </View>
     )
 }
+
+const s = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#676767",
+    },
+    Text: {
+        color: "#f1f1f1",
+        fontSize: 60,
+    }
+})
